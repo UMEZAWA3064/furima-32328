28c6742c4642b6cd24468410327997b339d62308
UMEZAWA3064 <umezawayuna@icloud.com>
2021-03-01 05:23:43 +0900

    userモデルのテスト

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,12 @@
 class ApplicationController < ActionController::Base
-
-  
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-
   private
-  def configure_permitted_parameters        #データベースに保存できるように設定
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :email, :encrypted_password, :family_name, :first_name, :family_name_kana, :first_name_kana, :birth_date])
+
+  # データベースに保存できるように設定
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up,
+                                      keys: [:nickname, :family_name, :first_name,
+                                             :family_name_kana, :first_name_kana, :birth_date])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,15 +4,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         
-         
-   #空だと保存できないバリデーション      
-    validates :nickname, presence: true
-    validates :email, uniqueness: true
-    validates :encrypted_password, presence: true
-    validates :family_name, presence: true
-    validates :first_name, presence: true
-    validates :family_name_kana, presence: true
-    validates :first_name_kana, presence: true
-    validates :birth_date, presence: true
+  # 空だと保存できないバリデーション
+  validates :nickname, presence: true
+  validates :email, uniqueness: { case_sensitive: true }
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}+\z/i
+  validates :password, :password_confirmation, length: { minimum: 6 },
+                       format: { with: VALID_PASSWORD_REGEX }
+
+  validates :family_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :family_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :birth_date, presence: true
 end
