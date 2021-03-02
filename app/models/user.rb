ce90cca1744ce0a49736d3ca2c2f69f47e027876
UMEZAWA3064 <umezawayuna@icloud.com>
2021-03-03 04:05:06 +0900

    with_optionsでまとめる

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,15 +5,18 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   # 空だと保存できないバリデーション
-  validates :nickname, presence: true
-  validates :email, uniqueness: { case_sensitive: true }
-  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}+\z/i
-  validates :password, :password_confirmation, length: { minimum: 6 },
-                       format: { with: VALID_PASSWORD_REGEX }
-
-  validates :family_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/ }
-  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/ }
-  validates :family_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
-  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
-  validates :birth_date, presence: true
+  with_options presence: true do
+    
+    validates :nickname
+    validates :email, uniqueness: { case_sensitive: true }
+    VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}+\z/i
+    validates :password, :password_confirmation, length: { minimum: 6 },
+    format: { with: VALID_PASSWORD_REGEX }
+    
+    validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/ }
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/ }
+    validates :family_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+    validates :birth_date
+  end
 end
