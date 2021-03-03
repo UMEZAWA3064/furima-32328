8a76edbde8955c957c26b7dd65434d05cf8ce73a
UMEZAWA3064 <umezawayuna@icloud.com>
2021-03-04 04:51:35 +0900

    正規表現をwith_optionsでまとめる

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,17 +6,22 @@ class User < ApplicationRecord
 
   # 空だと保存できないバリデーション
   with_options presence: true do
-    
     validates :nickname
+    validates :birth_date
     validates :email, uniqueness: { case_sensitive: true }
+    
     VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}+\z/i
     validates :password, :password_confirmation, length: { minimum: 6 },
     format: { with: VALID_PASSWORD_REGEX }
-    
-    validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/ }
-    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/ }
-    validates :family_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
-    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
-    validates :birth_date
+   
+    with_options format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/ } do
+      validates :family_name
+      validates :first_name 
+    end
+
+    with_options format: { with: /\A[ァ-ヶー－]+\z/ } do
+      validates :family_name_kana
+      validates :first_name_kana
+    end
   end
 end
