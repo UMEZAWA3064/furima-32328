c2715bbcca7f2eabc4ab299d4bd8eda706ee5c72
UMEZAWA3064 <umezawayuna@icloud.com>
2021-03-07 06:38:44 +0900

    Active Storageの導入

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+         has_many :items
+
   # 空だと保存できないバリデーション
   with_options presence: true do
     validates :nickname
