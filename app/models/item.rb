2c127a4342e7de0a491dcd0f6f9727470e66e689
UMEZAWA3064 <umezawayuna@icloud.com>
2021-03-08 04:56:14 +0900

    ActiveHashを導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,13 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :prefecture
+
+      #空では保存できないようにする
+  validates :name, :description, :price, presence: true
+
+      #ジャンルの選択が「--」の時は保存できないようにする
+  validates :prefecture_id, numericality: { other_than: 0 } 
 end
