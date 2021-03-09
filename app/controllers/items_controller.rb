435f80ffad6bd0614e70aae9fa9299ac91ce30a0
UMEZAWA3064 <umezawayuna@icloud.com>
2021-03-09 19:18:22 +0900

    発送元地域とカテゴリーのプルダウンメニュー

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,11 +2,19 @@ class ItemsController < ApplicationController
   def index
   end
 
+  def new
+    @item = Item.new
+  end
+
+  def create
+    Item.create(item_params)
+  end
+
 
   private
 
   def item_params
-    params.require(:item).permit(:description, :image).merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :price, :description, :image).merge(user_id: current_user.id)
 
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -4,9 +4,10 @@ class Item < ApplicationRecord
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :prefecture
+  belongs_to :category
 
       #空では保存できないようにする
-  validates :name, :description, :price, presence: true
+  validates :name, :description, :price, :category, presence: true
 
       #ジャンルの選択が「--」の時は保存できないようにする
   validates :prefecture_id, numericality: { other_than: 0 } 
