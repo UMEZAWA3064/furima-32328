c4b18ac6e78389bcb2c5ffe97ce86c8422667246
UMEZAWA3064 <umezawayuna@icloud.com>
2021-03-10 03:36:32 +0900

    商品状態のプルダウンメニュー

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,10 +5,11 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :prefecture
   belongs_to :category
+  belongs_to :item_status
 
       #空では保存できないようにする
-  validates :name, :description, :price, :category, presence: true
+  validates :name, :description, :price, :category, :item_status, presence: true
 
       #ジャンルの選択が「--」の時は保存できないようにする
-  validates :prefecture_id, numericality: { other_than: 0 } 
+  validates :prefecture_id, :category_id, :item_status, numericality: { other_than: 0 } 
 end
