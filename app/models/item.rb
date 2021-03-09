c8d3967782f5f7a6fee12ece226e97af1c428b2d
UMEZAWA3064 <umezawayuna@icloud.com>
2021-03-10 04:17:37 +0900

    配送料の負担、発送までの日数のプルダウンメニュー

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,10 +6,12 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :category
   belongs_to :item_status
+  belongs_to :delivery_burden
+  belongs_to :delivery_day
 
       #空では保存できないようにする
-  validates :name, :description, :price, :category, :item_status, presence: true
+  validates :name, :description, :price, :category, :item_status, :delivery_burden, :delivery_day, presence: true
 
       #ジャンルの選択が「--」の時は保存できないようにする
-  validates :prefecture_id, :category_id, :item_status, numericality: { other_than: 0 } 
+  validates :prefecture_id, :category_id, :item_status, :delivery_burden, :delivery_day, numericality: { other_than: 0 } 
 end
