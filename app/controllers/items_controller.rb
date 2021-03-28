68a0f236189d0634a82a10d03a8ffed603591ecd
UMEZAWA3064 <umezawayuna@icloud.com>
2021-03-28 21:50:43 +0900

    URL直打ち禁止

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,11 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]  # exceptでログインしていない状態でもトップページ、詳細ページ、新規登録、ログインページに飛べる
 
+  before_action :move_to_index, except: [:index, :show]       #未ログインユーザーが投稿画面などに直接アクセスしてきたらindexに遷移するようにする。index,showは除外
+  before_action :correct_user,only: [:edit, :destroy]                     #URL直打ち禁止にする。他のユーザーが編集、削除できないようにする。
+
+
+
   def index
     @items = Item.all.order(created_at: :desc)
   end
@@ -26,10 +31,32 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def update
+    item = Item.find(params[:id])
+    if item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit
+    end
+  end
+
+  def correct_user                         #他のユーザーがURLを直接打っても編集、削除できないようにする
+    @item = Item.find(params[:id])
+    unless @item.user.id == current_user.id
+  redirect_to action: :index
+    end
+  end
+
   private
 
   def item_params
     params.require(:item).permit(:name, :price, :description, :image, :category_id, :item_status_id, :prefecture_id,
                                  :delivery_burden_id, :delivery_day_id).merge(user_id: current_user.id)
+  end
+
+  def move_to_index                        #ログインしているユーザーでない時はindexに遷移する
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 end
