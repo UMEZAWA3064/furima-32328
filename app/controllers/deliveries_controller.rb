511ffcc480fd62ee9350a691d3f515cf870ffa2c
UMEZAWA3064 <umezawayuna@icloud.com>
2021-04-23 03:51:29 +0900

    各テーブルに情報を保存

--- app/controllers/deliveries_controller.rb
+++ app/controllers/deliveries_controller.rb
@@ -10,6 +10,7 @@ class DeliveriesController < ApplicationController
 
   def create
     @user_delivery = UserDelivery.new(delivery_params)
+    @item = Item.find(params[:item_id])
       if @user_delivery.valid?
         @user_delivery.save
         redirect_to root_path
@@ -20,6 +21,6 @@ class DeliveriesController < ApplicationController
 
   private
   def delivery_params
-    params.permit(:post_code, :prefecture_id, :city, :address, :building_name, :phone_number, :user_id, :item_id)
+    params.require(:user_delivery).permit(:post_code, :prefecture_id, :city, :address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 end
--- app/models/user_delivery.rb
+++ app/models/user_delivery.rb
@@ -13,7 +13,7 @@ class UserDelivery
   validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
 
   def save
+    buyer = Buyer.create(user_id: user_id, item_id: item_id)
     Delivery.create(post_code: post_code, prefecture_id: prefecture_id, city: city, address: address, building_name: building_name, phone_number: phone_number,  building_name: building_name, buyer_id: buyer.id)
-    buyer = Buyer.create(user_id: user_id, buyer_id: buyer_id)
   end
 end
