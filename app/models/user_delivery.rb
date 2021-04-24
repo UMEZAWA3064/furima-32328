89aac7bf77d5cea7ac2a8ca9a1f921db45f6f59b
UMEZAWA3064 <umezawayuna@icloud.com>
2021-04-25 07:25:20 +0900

    テストコード

--- app/models/user_delivery.rb
+++ app/models/user_delivery.rb
@@ -3,14 +3,14 @@ class UserDelivery
   attr_accessor :post_code, :prefecture_id, :city, :address, :building_name, :phone_number, :user_id, :item_id
 
   with_options presence: true do
-    validates :city, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
+    validates :city, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
     validates :address
-    validates :phone_number, format: { with: /\A0(\d{1}[-(]?\d{4}|\d{2}[-(]?\d{3}|\d{3}[-(]?\d{2}|\d{4}[-(]?\d{1})[-)]?\d{4}\z|\A0[5789]0[-]?\d{4}[-]?\d{4}\z/, message: "is invalid. Include hyphen(-)" }
+    validates :phone_number, format: { with: /\A\d{10}$|^\d{11}\z/}
     validates :user_id
     validates :item_id
-    validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)" }
+    validates :post_code, format: { with: /\A\d{3}[-]\d{4}$|^\d{3}[-]\d{2}$|^\d{3}\z/ }
   end
-  validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
+  validates :prefecture_id, numericality: { other_than: 0 }
 
   def save
     buyer = Buyer.create(user_id: user_id, item_id: item_id)
---
+++ spec/models/user_delivery_spec.rb
@@ -0,0 +1,92 @@
+require 'rails_helper'
+
+RSpec.describe UserDelivery, type: :model do
+  describe '#create' do
+    before do
+      @user_delivery = FactoryBot.build(:user_delivery)
+    end
+  
+    describe '商品出品' do
+      context '商品が購入できるとき' do
+        it '全てが存在するとき' do
+          expect(@user_delivery).to be_valid
+        end
+
+        it 'post_codeが半角数字であれば購入できる' do
+          @user_delivery.post_code = '123-4567'
+          expect(@user_delivery).to be_valid
+        end
+
+        it 'phone_numberが半角数字であれば登録できる' do
+          @user_delivery.phone_number = '09011112222'
+          expect(@user_delivery).to be_valid
+        end
+
+
+        context '商品が購入できないとき' do
+          it 'post_codeが空だと購入できない' do
+            @user_delivery.post_code = ''
+            @user_delivery.valid?
+            expect(@user_delivery.errors.full_messages).to include("Post code can't be blank")
+          end
+
+          it 'post_codeが半角数字でないと購入できない' do
+            @user_delivery.post_code = '１２３-４５６７'
+            @user_delivery.valid?
+            expect(@user_delivery.errors.full_messages).to include("Post code is invalid")
+          end
+
+          it 'post_codeにハイフンがなければ購入できない' do
+            @user_delivery.post_code = '1234567'
+            @user_delivery.valid?
+            expect(@user_delivery.errors.full_messages).to include("Post code is invalid")
+          end
+
+          it 'prefecture_idが空だと購入できない' do
+            @user_delivery.prefecture_id = nil
+            @user_delivery.valid?
+            expect(@user_delivery.errors.full_messages).to include("Prefecture is not a number")
+          end
+
+          it 'cityが空だと購入できない' do
+            @user_delivery.city = ''
+            @user_delivery.valid?
+            expect(@user_delivery.errors.full_messages).to include("City can't be blank")
+          end
+
+          it 'addressが空だと購入できない' do
+            @user_delivery.address = ''
+            @user_delivery.valid?
+            expect(@user_delivery.errors.full_messages).to include("Address can't be blank")
+          end
+
+          it 'phone_numberが空だと購入できない' do
+            @user_delivery.phone_number = ''
+            @user_delivery.valid?
+            expect(@user_delivery.errors.full_messages).to include("Phone number can't be blank")
+          end
+
+          it 'phone_numberにハイフンがあると購入できない' do
+            @user_delivery.phone_number = '090-1111-2222'
+            @user_delivery.valid?
+            expect(@user_delivery.errors.full_messages).to include("Phone number is invalid")
+          end
+
+          it 'phone_numberが12桁だと購入できない' do
+            @user_delivery.phone_number = '090111122222'
+            @user_delivery.valid?
+            expect(@user_delivery.errors.full_messages).to include("Phone number is invalid")
+          end
+          
+          it 'phone_numberが9桁以下だと購入できない' do
+            @user_delivery.phone_number = '04811122'
+            @user_delivery.valid?
+            expect(@user_delivery.errors.full_messages).to include("Phone number is invalid")
+          end
+
+        end
+      end
+    end
+  end
+end
+
